2236de1e929e278ca0c9df762d1aa14b23ea9ef7
Paraizo <eduardoparize.05@gmail.com>
2025-08-04 08:47:18 -0300

    atuliazando branch

--- editar.js
+++ editar.js
@@ -1,6 +1,2 @@
 import { exibirMenu } from "./spacegirlsMOD.js";
-import { createInterface } from "readline";
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+
--- filtrarPrioridade.js
+++ filtrarPrioridade.js
@@ -1,50 +1,43 @@
-import { exibirMenu } from "./spacegirlsMOD.js";
-import { createInterface } from "readline";
-const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+import { exibirMenu, prompt } from "./spacegirlsMOD.js";
 
 import { missoes } from "./spacegirlsMOD.js";
 
 export function filtrarPrioridade() {
-    console.clear();
-    console.log(
-      "Filtre as missões por prioridade\nAs PRIORIDADES vão de 1 a 5, com importância crescente.\n"
-    );
-    rl.question("Digite a prioridade desejada: ", (filtro) => {
-      filtro = parseInt(filtro, 10);
-      if (isNaN(filtro) || filtro < 1 || filtro > 5) {
-        console.log(
-          "Prioridade inválida.\nDigite um número de 1 a 5, por favor."
-        );
-        filtrarPrioridade();
-        return;
-      }
-      if (missoes.length == 0) {
-        console.log("Não há missões no momento. Voltando ao menu.");
-        exibirMenu();
-        return;
-      }
-      const missoesFiltradas = missoes.filter(
-        (missao) => missao.Prioridade === filtro
-      );
-      if (missoesFiltradas.length > 0) {
-        console.clear();
-        console.log(`======-MISSÕES COM PRIORIDADE ${filtro}-======`);
-        missoesFiltradas.forEach((missao, index) => {
-          console.log(
-            `Nº da Missão: ${index + 1} || Prioridade: ${
-              missao.Prioridade
-            } | Nome: ${missao.Nome}  | Destino: ${
-              missao.Destino
-            } | Tripulante: ${missao.tripulantes}`
-          );
-        });
-      } else {
-        console.clear();
-        console.log("Nenhuma missão com a prioridade " + filtro + " encontrada.");
-      }
-      exibirMenu();
-    });
+  console.clear();
+  console.log(
+    "Filtre as missões por prioridade\nAs PRIORIDADES vão de 1 a 5, com importância crescente.\n"
+  );
+  let filtro = prompt("Digite a prioridade desejada: ");
+  filtro = parseInt(filtro, 10);
+  if (isNaN(filtro) || filtro < 1 || filtro > 5) {
+    console.log("Prioridade inválida.\nDigite um número de 1 a 5, por favor.");
+    filtrarPrioridade();
+    return;
   }
+  if (missoes.length == 0) {
+    console.log("Não há missões no momento. Voltando ao menu.");
+    exibirMenu();
+    return;
+  }
+  const missoesFiltradas = missoes.filter(
+    (missao) => missao.Prioridade === filtro
+  );
+  if (missoesFiltradas.length > 0) {
+    console.clear();
+    console.log(`======-MISSÕES COM PRIORIDADE ${filtro}-======`);
+    missoesFiltradas.forEach((missao, index) => {
+      console.log(
+        `Nº da Missão: ${index + 1} || Prioridade: ${
+          missao.Prioridade
+        } | Nome: ${missao.Nome}  | Destino: ${missao.Destino} | Tripulante: ${
+          missao.tripulantes
+        }`
+      );
+    });
+  } else {
+    console.clear();
+    console.log("Nenhuma missão com a prioridade " + filtro + " encontrada.");
+  }
+  prompt("\nPressione Enter para voltar ao menu...");
+  return;
+}
